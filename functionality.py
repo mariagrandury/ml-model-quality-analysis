37e9476870ee16999a62e3aa7fbb3b322b998b86
Maria Grandury <mariagrandury@gmail.com>
2020-11-21 18:06:29 +0100

    Performance analysis for binary and multiclass classification problems

--- functionality.py
+++ functionality.py
@@ -8,15 +8,22 @@ import tensorflow as tf
 
 class FunctionalityAnalysis:
 
-    def __init__(self, y_true, y_pred):
+    def __init__(self, y_true, y_pred, task, nclass):
         self.y_true = y_true
         self.y_pred = y_pred
+        self.task = task
+        self.nclass = nclass
+
+    def show_matrix_confusion(self):
+        return tf.math.confusion_matrix(self.y_true, self.y_pred).numpy()
 
     def calculate_accuracy(self):
         acc = tf.keras.metrics.Accuracy()
         acc.update_state(self.y_true, self.y_pred)
         accuracy = acc.result().numpy()
         return accuracy
+
+    # Check if balanced or imbalanced data
 
     def calculate_precision(self):
         precision = tf.keras.metrics.Precision()
@@ -36,5 +43,16 @@ class FunctionalityAnalysis:
         f1_score = 2 * precision * recall / (precision + recall)
         return f1_score
 
-    def show_matrix_confusion(self):
-        return tf.math.confusion_matrix(self.y_true, self.y_pred).numpy()
+    def evaluate_performance(self):
+        if self.task == 'classification':
+            self.show_matrix_confusion()
+            print('Confusion Matrix: \n', self.show_matrix_confusion(), '\n')
+            if self.nclass == 'binary':
+                print('Accuracy:  ', self.calculate_accuracy(), '\n')
+                print('Precision: ', self.calculate_precision())
+                print('Recall:    ', self.calculate_recall())
+                print('F1 Score:  ', round(self.calculate_f1_score(), 2))
+            else:
+                print('Accuracy:  ', self.calculate_accuracy(), '\n')
+        else:
+            print('Implement evaluation for regression problems!')
