66acb8e26bb29d07063ab4384f98a4f7ea068561
Maria Grandury <mariagrandury@gmail.com>
2020-11-23 01:12:50 +0100

    Create adversarial examples

--- model_image.py
+++ model_image.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 
 from data import download_data
-
+from robustness import get_gradient_sign
+import matplotlib.pyplot as plt
 
 class ModelImage:
 
@@ -24,7 +25,10 @@ class ModelImage:
         self.model = tf.keras.models.load_model(model_path)
         self.input_shape = self.model.get_layer(self.model.layers[0].name).input.shape
         self.num_classes = self.model.get_layer(self.model.layers[-1].name).output.shape[1]
-        print('Model loaded! Input shape:', self.input_shape, ', number of classes:', self.num_classes)
+        if self.num_classes == 1: self.num_classes = 2
+        #print('Model loaded! Input shape:', self.input_shape, ', number of classes:', self.num_classes)
+
+        return self
 
 
     def load_data(self, split=0.1, test_samples=100,
@@ -60,7 +64,7 @@ class ModelImage:
 
             test_ds = tf.keras.preprocessing.image_dataset_from_directory(
                 data_dir, validation_split=split, subset='validation', seed=seed,
-                image_size=size, batch_size=batch_size, shuffle=shuffle
+                image_size=size, batch_size=1, shuffle=shuffle
             )
             data = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
@@ -70,17 +74,19 @@ class ModelImage:
             split = "train[:" + str(test_samples) + "]"
             test_ds = tfds.load(data_tf, split=split, as_supervised=True, shuffle_files=shuffle)
             test_ds = test_ds.map(lambda x, y: (tf.image.resize(x, size), y))
-            data = test_ds.cache().batch(batch_size).prefetch(buffer_size=AUTOTUNE)
+            data = test_ds.cache().batch(1).prefetch(buffer_size=AUTOTUNE)
 
 
         self.data = data
-        print('Data loaded! ', data)
+        #print('Data loaded! ', data)
+
+        return self
 
 
     def predict(self):
         """Generate the model predictions for the given data.
 
-        :returns: predictions: Predictions of the model. Numpy array of shape (1,).
+        :returns: predictions: Predictions of the model.
         :returns: labels: Labels to perform functionality analysis.
         """
         predictions= []
@@ -108,3 +114,43 @@ class ModelImage:
                 y_true.append(label)
 
         return y_pred, y_true
+
+    def robustness(self):
+        y_true = []
+        y_pred = []
+        y_adv = []
+
+        for image, label in self.data.take(1):
+            image = image/ 255
+            plt.imshow(image[0]*0.5+0.5)
+            plt.show()
+
+            y_true.append(label.numpy()[0])
+            y_pred.append(tf.argmax(self.model.predict(image)[0]).numpy())
+
+            model_pred = self.model.predict(image)
+            print(model_pred)
+            label = tf.one_hot(label, model_pred.shape[-1])
+            label = tf.reshape(label, (1, model_pred.shape[-1]))
+            print(label)
+            perturbations = get_gradient_sign(self.model, image, label)
+            #print(perturbations)
+            plt.imshow(perturbations[0]*0.5+0.5)
+            plt.show()
+            #print_perturbations = tf.reshape(perturbations[0], size)
+            #plt.imshow(print_perturbations)
+
+            adv_x = image + 0.05 * perturbations
+            adv_x = tf.clip_by_value(adv_x, -1, 1)
+            print(adv_x.shape)
+            print(self.model.predict(adv_x))
+            adv_label = tf.argmax(self.model.predict(adv_x)[0]).numpy()
+            print(adv_label)
+            y_adv.append(adv_label)
+
+            plt.figure()
+            #adv_x = tf.reshape(adv_x, size)
+            plt.imshow(adv_x[0]*0.5+0.5)
+            plt.show()
+
+        return y_true, y_pred, y_adv
