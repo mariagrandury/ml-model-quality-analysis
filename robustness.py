e2aabfc8d1daa4daa4c74c73db4e6699a56e671e
Maria Grandury <mariagrandury@gmail.com>
2020-11-24 02:24:58 +0100

    Update loss function

--- robustness.py
+++ robustness.py
@@ -2,12 +2,11 @@ import tensorflow as tf
 
 
 def get_gradient_sign(model, input_image, input_label):
-    loss_object = tf.keras.losses.CategoricalCrossentropy()
 
     with tf.GradientTape() as tape:
         tape.watch(input_image)
         prediction = model(input_image)
-        loss = loss_object(input_label, prediction)
+        loss = tf.keras.losses.categorical_crossentropy(input_label, prediction, from_logits=True)
 
     gradient = tape.gradient(loss, input_image)
 
